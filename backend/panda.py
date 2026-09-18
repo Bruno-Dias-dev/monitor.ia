d34f56f007b60d2aece2dbcf94ba59d785aa90cb
Bruno Dias <hamiltonbrunodias@email.com>
2026-09-18 14:39:35 -0300

    criação de rota e captura de parâmetro na planilha

--- backend/panda.py
+++ backend/panda.py
@@ -3,16 +3,47 @@ import requests
 import pandas as pd
 from io import BytesIO
 from dotenv import load_dotenv
+from flask import Flask, jsonify
 
+# Rodando variáveis de api
 load_dotenv()
-
 url = os.getenv("EXCEL_URL")
 
-response = requests.get(url)
-response.raise_for_status()
-# df = pd.read_excel(BytesIO(response.content))
-# pd.set_option("display.max_columns", None)
+# Captura do Excel
+def capturaExcel():
+    response = requests.get(url)
+    response.raise_for_status()
+    df = pd.read_excel(BytesIO(response.content))
 
-df_avaliacoes = pd.read_excel(BytesIO(response.content), sheet_name="media")
+#   pd.set_option("display.max_columns", None)
 
-print(df_avaliacoes)
+    df_avaliacoes = pd.read_excel(BytesIO(response.content), sheet_name="media")
+
+    # Barbara Aparecyda
+    barbara = df_avaliacoes[df_avaliacoes["Contemplado"] == "Barbara Aparecyda"]
+    nomeBarbara = barbara["Contemplado"].iloc[0]
+    mediaSaudacao = barbara["Média Saudação"].iloc[0]
+    mediaConhecimento = barbara["Média Clareza"].iloc[0]
+    mediaResolucao = barbara["Média Conhecimento"].iloc[0]
+    mediaEmpatia = barbara["Média Empatia"].iloc[0]
+
+    return {
+        "nome1": nomeBarbara,
+        "Média saudacao Barbara": mediaSaudacao,
+        "Média Conhecimento Barbara": mediaConhecimento,
+        "Média Resolução Barbara": mediaResolucao,
+        "Media Empatia Barbara": mediaEmpatia
+    }
+
+
+app = Flask(__name__)
+
+@app.route("avaliacoes/media", methods=["GET"])
+
+    dados = capturaExcel()
+
+    return jsonify(dados)
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
