a151a979538bf83a446e59dd70f3af952ed4edb2
Bruno Dias <hamiltonbrunodias@email.com>
2026-09-24 18:28:14 -0300

    ajuste final

--- backend/dadosomni.py
+++ backend/dadosomni.py
@@ -1,20 +1,23 @@
 from flask import Flask, jsonify, request
-from flask_jwt_extended import jwt_required
+from flask_jwt_extended import jwt_required, get_jwt_identity
 from flask_cors import CORS
 from chatwoot import buscar_chatwoot
 
 app = Flask(__name__)
-
 CORS(app)
 
 @app.route("/api/dashboard", methods=["GET"])
-# ,@jwt_required()
+@jwt_required()
 
 def dashboard():
+
+    usuario_id = get_jwt_identity()
+
     chatwoot = buscar_chatwoot()
 
 
     return jsonify({
+        "usuario_id": usuario_id,
         "chatwoot": chatwoot
     })
 
